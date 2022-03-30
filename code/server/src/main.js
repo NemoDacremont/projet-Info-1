b1d6f46933a27463f77b9813eb5659196d516ad2
odasta <nemo.dacremont@gmail.com>
2022-03-30 18:43:42 +0200

    Création du serveur

--- code/server/src/main.js
+++ code/server/src/main.js
@@ -1,4 +1,23 @@
 
-/*
-	Bonjour dani
-*/
+const http = require("http");
+
+const app = http.createServer();
+
+app.on("request", (req, res) => {
+	res.writeHead(400);
+
+	// Sert à stocker les chunks
+	const data = []
+	req.on("data", (chunk) => {
+		data.push(chunk.toString("utf-8"));
+	});
+
+
+	req.on("end", () => {
+		const text = data.join("");
+		console.log(text);
+		res.end();
+	})
+});
+
+app.listen(8080);
