603e5dac288a576ee9598e3daecfcb82d043d0a3
odasta <nemo.dacremont@gmail.com>
2022-04-04 18:27:21 +0200

    Update index.js
    Corrected the file name in the import

--- code/server/src/routes/index.js
+++ code/server/src/routes/index.js
@@ -1,6 +1,6 @@
 
 const sauvegarde = require("./sauvegarde_fichier")
-const telecharge = require("./telecharge")
+const telecharge = require("./telecharge_fichier")
 
 module.exports = [
 	sauvegarde,
