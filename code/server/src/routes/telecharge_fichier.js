0b98d61ceb465877aaa28f11f4e6704f5f262416
odasta <nemo.dacremont@gmail.com>
2022-04-05 13:44:00 +0200

    Update telecharge_fichier.js
    Ajoute le nom du fichier dans la réponse à /telecharge, le fichier téléchargé sera maintenant nommé data.zip plutôt que "telecharge"

--- code/server/src/routes/telecharge_fichier.js
+++ code/server/src/routes/telecharge_fichier.js
@@ -10,11 +10,14 @@ module.exports = {
 
 	// 
 	handler: async (req, res) => {
-		res.writeHead(200);
-
 		const { chemin_zip } = await cree_zip(data_dir);
 
 		const read_stream = fs.createReadStream(chemin_zip);
+
+		res.writeHead(200, {
+			"Content-Disposition": "attachment;filename=" + "data.zip",
+      'Content-Type': 'application/zip'
+		});
 
 		read_stream.pipe(res);
 		read_stream.on("close", () => {
