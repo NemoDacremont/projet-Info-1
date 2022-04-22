0131d8bf23d05bbc72dfa38e4bedc83457785198
odasta <nemo.dacremont@gmail.com>
2022-04-22 18:20:29 +0200

    Update sauvegarde_local.py
    Ajout du séparateur et du docstring de la fonction data_to_string servant à convertir des données CSV en un string au format CSV

--- code/virus/sauvegarde_local.py
+++ code/virus/sauvegarde_local.py
@@ -1,20 +1,26 @@
 
-def data_to_string(data):
+def data_to_string(data, separator=","):
 	"""
+		Entrée: - data: au format de liste de listes correspondant à des données CSV
+						- separator: string du séparateur 
+		Retourne: string au format CSV de data
 	"""
+	# liste des lignes du fichier
 	tmp = []
 	for i in range(len(data)):
+		# Stocke les données avant de les concaténées avec un séparateur
 		a = []
 		for j in range(len(data[i])):
 			a.append(str(data[i][j]))
-		tmp.append(",".join(a))
+
+		tmp.append(separator.join(a))
 
 	return "\n".join(tmp)
 
 
 def sauvegarde(chemin, data):
 	""" auteur: Nemo Anael
-		Entrées: - data: 
+		Entrées: - data: string
 		Retourne: Booléen, True si la sauvegarde a réussie, False sinon
 
 		Sauvegarde au format CSV, séparateur: ','
