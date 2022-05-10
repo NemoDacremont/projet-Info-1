0a4274bd58e436948ac1f95ba45cf8853af4acf8
odasta <67148727+odasta@users.noreply.github.com>
2022-05-10 17:42:12 +0200

    Ajout de traitement/config.json
    Ajout d'un fichier de configuration

--- code/traitement/main.py
+++ code/traitement/main.py
@@ -1,17 +1,33 @@
 
+# importation des dépendences
+import os
+import json
+
+# Importation des fichiers locaux
 import refine
 import manipule_csv
 import traitement
+import recherche
+
+dossier_courant = os.path.dirname(__file__)
+chemin_config = f"{dossier_courant}/config.json"
+
+config_file = open(chemin_config)
+config = json.load(config_file)
+config_file.close()
+
+ip_serveur = config["ip"]
 
 liste_des_mdp = manipule_csv.parse_CSV("./top12k_pass.txt")
 liste_des_mdp = refine.extract(liste_des_mdp, 0)
 
-csv = manipule_csv.parse_CSV("./sauvegarde_locale.csv")
-
+# Actualise les données depuis le serveur, sauvegardée dans le dossier ./data
+manipule_csv.download_from_server(ip_serveur)
+csv = manipule_csv.parse_CSV("./data")
 
 csv = refine.fine(csv)
 csv = refine.fine_accent(csv)
 csv = refine.fine_backspace(csv)
 csv = refine.fine_str(csv)
 
-print(traitement.brute_force(csv, liste_des_mdp))
+print(recherche.brute_force(csv, liste_des_mdp))
